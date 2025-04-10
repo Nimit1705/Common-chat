9d5b7d20f6803db1648321087d12df0c327074a6
Nimit <nimitjainsogani2003@gmail.com>
2025-04-10 21:53:38 +0530

    Updated: port

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const timeFunc = () => {
 }
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = new createServer(app);
 const io = new Server(server, {
     cors:{
